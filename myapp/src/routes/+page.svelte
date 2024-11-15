03003d8f330aacb814944d0b5005b785d84553c8
TristanKatte <tristankatte@hotmail.com>
2024-11-15 20:31:09 +0100

    .

--- myapp/src/routes/+page.svelte
+++ myapp/src/routes/+page.svelte
@@ -22,7 +22,10 @@
     main {
         width: 100%;
         height: 100vh;
+        display: flex;
+        justify-content: center;
         background-color: var(--main-bg-color);
+
     }
 
 </style>
