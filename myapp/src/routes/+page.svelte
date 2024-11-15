c11720cfb84b64565423c7deff0df1fdeeedcf96
TristanKatte <tristankatte@hotmail.com>
2024-11-15 14:36:18 +0100

    .

--- myapp/src/routes/+page.svelte
+++ myapp/src/routes/+page.svelte
@@ -13,7 +13,15 @@
         --heading-small: #008080;
     }
 
+    * {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+    }
+
     main {
+        width: 100%;
+        height: 100vh;
         background-color: var(--main-bg-color);
     }
 
