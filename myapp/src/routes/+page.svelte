b44a403927a49884f09620be2c12e800f46a9d5a
TristanKatte <tristankatte@hotmail.com>
2024-11-15 13:36:42 +0100

    custom properties prepared

--- myapp/src/routes/+page.svelte
+++ myapp/src/routes/+page.svelte
@@ -1,2 +1,20 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<main>
+
+
+</main>
+
+
+
+<style>
+    :root {
+        --main-bg-color: #4b4f58;
+        --btn-color: #00adb5;
+        --txt-color: #c2c2c2;
+        --heading-small: #008080;
+    }
+
+    main {
+        background-color: var(--main-bg-color);
+    }
+
+</style>
