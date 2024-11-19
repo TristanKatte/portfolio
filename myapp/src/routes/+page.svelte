15e66afd4ea6a8cf941023bed91cdc56255e745b
TristanKatte <tristankatte@hotmail.com>
2024-11-19 20:31:30 +0100

    added import statements

--- myapp/src/routes/+page.svelte
+++ myapp/src/routes/+page.svelte
@@ -1,5 +1,9 @@
 <script>
- 
+  import Header from './components/Header.svelte'
+  import Footer from './components/Footer.svelte'
+  import Home from './components/Home.svelte'
+  import About from './components/About.svelte'
+  import Contact from './components/Contact.svelte'
 
 </script>
 
