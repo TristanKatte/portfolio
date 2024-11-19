24aad088385596c8a6abb2919bfcba5a23201067
TristanKatte <tristankatte@hotmail.com>
2024-11-19 19:57:21 +0100

    .

---
+++ myapp/src/App.svelte
@@ -0,0 +1,8 @@
+<script>
+    import { Router, Route, link } from 'svelte-routing';
+    import Header from './components/Header.svelte';
+    import Home from './components/Home.svelte';
+    import About from './components/About.svelte';
+    import Projects from './components/Projects.svelte';
+    import Contact from './components/Contact.svelte';
+</script>
---
+++ myapp/src/routes/components/About.svelte
@@ -0,0 +1,8 @@
+<section>
+    <h2>About Me</h2>
+    <p>Write a brief introduction about yourself here.</p>
+</section>
+
+<style>
+    /* Add your styles here */
+</style>
