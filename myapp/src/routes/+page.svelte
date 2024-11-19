61637246750f6b0e13a6a86650e36c60a317e117
TristanKatte <tristankatte@hotmail.com>
2024-11-19 15:18:26 +0100

    update

--- myapp/src/routes/+page.svelte
+++ myapp/src/routes/+page.svelte
@@ -20,47 +20,77 @@
 
 <style>
     :root {
+        /* colors */
         --main-bg-color: #4b4f58;
         --btn-color: #00adb5;
         --txt-color: #c2c2c2;
-        --heading-small: #008080;
+        --heading-color: #008080;
+
+        /* headings */
+        --heading-large: 4.5em;
+        --heading-medium: 2.5em;
+        --heading-small: 1.5em;
+        --body-txt:1.3em;
     }
 
     @font-face {
-        
+    font-family: "Nunito", sans-serif;
+    src: url(/myapp/static/fonts/NunitoSans-VariableFont_YTLC\,opsz\,wdth\,wght.ttf);
+    }
+
+    @font-face {
+    font-family: "Inconsolata", sans-serif;
+    src: url(/myapp/static/fonts/Inconsolata[wdth\,wght].ttf);
     }
 
 
-    
-    * {
-    margin: 0;
-    padding: 0;
-    box-sizing: border-box;
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  scroll-behavior: smooth;
+  scrollbar-width: none;
     }
 
-    main {
-    width: 100%;
-    height: 100vh;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    margin: auto;
-    background-color: var(--main-bg-color);
-    overflow: hidden;
+main {
+  display: flex;
+  justify-content: center;
+  width: 100%;
+  height: 100vh;
+  margin: auto;
+  letter-spacing: 0.5px;
+  background-color: var(--main-bg-color);
     }
 
-    section {
-    display: flex;
-    flex-direction: column;
-    width: 90%;
-    max-width: 800px;
-    margin: auto;
-    padding: 20px;
-    color: var(--txt-color);
-    }
+section {
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  height: 100vh;
+  max-width: 800px;
+  margin: auto;
+  padding: 20px;
+  color: var(--txt-color);
+    } 
 
-    h1, h2 {
-    color: var(--heading-small);
+h1, h2 {
+  color: var(--heading-color);
+  font-family: "Nunito", sans-serif;
+  margin: .3em;
+  text-align: left;
+}
+
+h1 {
+  font-size: var(--heading-large);
+}
+
+h2 {
+    font-size: var(--heading-medium);
+}
+
+p {
+    font-family: "Inconsolata", sans-serif;
+    font-size: var(--body-txt);
 }
 
 ul {
