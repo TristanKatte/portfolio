375f964bd51f5672625990461a249ef67673c54c
TristanKatte <tristankatte@hotmail.com>
2025-02-04 04:10:44 +0100

    added styles

---
+++ myapp/src/routes/+layout.svelte
@@ -0,0 +1,40 @@
+<script>
+    import Header from './components/Header.svelte'
+
+    import 'open-props/style'
+    import 'open-props/normalize'
+    import 'open-props/buttons'
+
+    import '../app.css'
+
+    let { children, data } = $props()
+</script>
+
+<section class="layout">
+    <Header />
+
+    <main>
+        
+    </main>
+</section>
+
+<style>
+    .layout {
+        height: 100%;
+        max-inline-size: 1440px;
+        display: grid;
+        grid-template-columns: auto 1fr auto;
+        margin-inline: auto;
+        padding-inline: var(--size-7);
+    }
+
+    main {
+        padding-block: var(--size-9);
+    }
+
+    @media (min-width: 1440px) {
+        .layout {
+            padding-inline: 0;
+        }
+    }
+</style>
