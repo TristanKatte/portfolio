c1485bc78020c329d68def76ca5c67b2ca22b1ba
TristanKatte <tristankatte@hotmail.com>
2025-02-11 16:16:57 +0100

    Update Header.svelte

--- myapp/src/routes/components/Header.svelte
+++ myapp/src/routes/components/Header.svelte
@@ -5,12 +5,11 @@
 <header>
     <img alt="personal logo van mij" src={Logo}>
     <nav class="effect">
-        
         <ul>
-        <li><a href="/">Home</a></li>
-        <li><a href="/about">About</a></li>
-        <li><a href="/projects">Projects</a></li>
-        <li><a href="/contact">Contact</a></li>
+            <li><a href="/">Home</a></li>
+            <li><a href="/about">About</a></li>
+            <li><a href="/projects">Projects</a></li>
+            <li><a href="/contact">Contact</a></li>
         </ul>
     </nav>
 </header>
