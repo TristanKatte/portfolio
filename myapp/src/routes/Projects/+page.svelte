bbe468b0ca5d66529a65948737134304629e6699
TristanKatte <tristankatte@hotmail.com>
2025-02-13 13:39:19 +0100

    .

--- myapp/src/routes/Projects/+page.svelte
+++ myapp/src/routes/Projects/+page.svelte
@@ -1,2 +1,14 @@
-<h1>About</h1>
-<p>This is a learning journal i made in the 2nd year of my study Frontend Design & Development at the AUAS</p>
+
+<main>
+    <h1>Projects</h1>
+    <p>This is where I showcase some of my best works</p>
+</main>
+<style>
+    h1 {
+        color: var(--heading-color);
+    }
+
+    p {
+        color: var(--txt-color);
+    }
+</style>
