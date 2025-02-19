f5a56f466edd09ee3e988f3367b1c835422275e0
TristanKatte <tristankatte@hotmail.com>
2025-02-19 15:46:16 +0100

    .

--- myapp/src/routes/+layout.svelte
+++ myapp/src/routes/+layout.svelte
@@ -31,7 +31,7 @@
         display: grid;
         margin-inline: auto;
         padding-inline: var(--size-7);
-        background-image: var(--gradient-23);
+        background-image: var(--gradient-8);
     }
 
     main {
