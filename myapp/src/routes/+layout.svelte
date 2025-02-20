6a7acfcd10e7885ff1d0208fe78bd64e43ef3aca
TristanKatte <tristankatte@hotmail.com>
2025-02-20 11:47:04 +0100

    added view transition

--- myapp/src/routes/+layout.svelte
+++ myapp/src/routes/+layout.svelte
@@ -1,24 +1,23 @@
 <script>
     import Footer from './components/Footer.svelte'
-	import Header from './components/Header.svelte'
+    import Header from './components/Header.svelte'
+    import Transition from './transition.svelte'
 
     import 'open-props/style'
-	import 'open-props/normalize'
-	import 'open-props/buttons'
+    import 'open-props/normalize'
+    import 'open-props/buttons'
 
     import '@fontsource/alata';
     import '@fontsource-variable/orbitron';
-
-    
 </script>
 
 <div class="layout">
     <Header />
     <main>
-
-        <slot />
+        <Transition>
+            <slot />
+        </Transition>
     </main>
-
     <Footer />
 </div>
 
@@ -30,25 +29,21 @@
 }
 
 .layout {
-        height: 100%;
-        max-width: 100%;
-        display: grid;
-        margin-inline: auto;
-        padding-inline: var(--size-7);
-        background-image: var(--gradient-8);
+    height: 100%;
+    max-width: 100%;
+    display: grid;
+    margin-inline: auto;
+    padding-inline: var(--size-7);
+    background-image: var(--gradient-8);
+}
+
+main {
+    padding-block: var(--size-0);
+}
+
+@media (min-width: 1440px) {
+    .layout {
+        padding-inline: 0;
     }
-
-
-
-
-    main {
-        padding-block: var(--size-0);
-        
-    }
-
-    @media (min-width: 1440px) {
-        .layout {
-            padding-inline: 0;
-        }
-    }
+}
 </style>
---
+++ myapp/src/routes/transition.svelte
@@ -0,0 +1,8 @@
+<script>
+  import { fade } from 'svelte/transition';
+  export let duration = 300;
+</script>
+
+<div in:fade={{ duration }} out:fade={{ duration }}>
+  <slot />
+</div>
