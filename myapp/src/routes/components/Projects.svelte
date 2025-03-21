b9979c0d02d3054017c30aff7c8744e242a124e5
TristanKatte <tristankatte@hotmail.com>
2025-03-21 15:30:31 +0100

    added stacking cards effect

--- myapp/src/routes/components/Projects.svelte
+++ myapp/src/routes/components/Projects.svelte
@@ -13,77 +13,106 @@
     <h2>My Projects</h2>
     <section class="projects-grid">
         <article class="project-card">
-            <a href="https://sprint-12-proof-of-concept-p6cy.onrender.com/" target="_blank">
-                    <h3>Fresk.Digital</h3>    
+            <section class="project-image">
                 <enhanced:img class="bright" src="/static/images/fresk-digital.png" alt="" />
-            </a>
-            <strong>NodeJS, EJS, Express</strong>
+            </section>
+            <section class="project-content">
+                <h3>Fresk.Digital</h3>
+                <p>Fresk.Digital was a project i did in the final sprint of my 1st year at the study Frontend Design & Development.
+                   Here, I made a website with <strong>NodeJS, EJS and Express</strong> to build a dashboard for the employees of Fresk.digital where they could see how many clicks they got and from what country those clicks
+                </p>
+                <a class="project-button" href="https://sprint-12-proof-of-concept-p6cy.onrender.com/" target="_blank">To project</a>
+            </section>
         </article>
-        
+
         <article class="project-card">
-            <a href="https://i-love-web-amber.vercel.app/" target="_blank">
-                    <h3>Digital garden</h3>
+            <section class="project-image">
                 <enhanced:img class="align-top" src="/static/images/digital-garden.png" alt="" />
-            </a>
-            <strong>Sveltekit, Markdown</strong>
+            </section>
+            <section class="project-content">
+                <h3>Digital garden</h3>
+                <p>A digital garden where i kept a journey from what i learned each sprint in my 3rd semester at the study Frontend Design & Development. Made with <strong>Sveltekit, Markdown</strong>
+                </p>
+                <a class="project-button" href="https://i-love-web-amber.vercel.app/" target="_blank">To project</a>
+            </section>
         </article>
-        
+
         <article class="project-card">
-            <a href="https://your-tribe-for-life-profile-card-liart.vercel.app/" target="_blank">
-                <h3>Visitekaartje</h3>
+            <section class="project-image">
+                <strong>(Only works on desktop)</strong>
+
                 <enhanced:img src="/static/images/visitekaartje.png" alt="">
-            </a>
-            <div>     
-                <strong>NodeJS, EJS, Express</strong>
-            </div>
-        </article>
-        
-        <article class="project-card">        
-            <a href="https://tristankatte.github.io/Brain-website/" target="_blank">
-                <h3>B-rain</h3>
-                <enhanced:img class="align-top" src="/static/images/b-rain.png" alt="" />
-            </a>
-            <strong>HTML, CSS, JS</strong>
-        </article>
-        
-        <article class=" project-card">
-            <a href="https://redpers.agency.fdnd.nl/" target="_blank">
-                <h3>Redpers</h3>
-                <enhanced:img src="/static/images/redpers.png" alt="" />
-            </a>
-            <strong>Sveltekit, WPAPI</strong>
+            </section>
+            <section class="project-content">
+                <h3>Digital business card</h3>
+                <p> A digital business card where i introduce myself and what my skills are. Made with <strong>Sveltekit, Directus</strong>
+                </p>
+                <a class="project-button" href="https://your-tribe-for-life-profile-card-liart.vercel.app/" target="_blank">To project</a>
+            </section>
         </article>
 
         <article class="project-card">
-           
-            <a href="https://tristankatte.github.io/Informaat-opdracht/" target="_blank">
-                <h3>Informaat</h3>
-                <enhanced:img src="/static/images/informaat.png" alt="" />
-            </a>
-            <div> 
-                <strong>HTML, CSS</strong>
-            </div>
+            <section class="project-image">
+                <enhanced:img src="/static/images/b-rain.png" alt="">
+            </section>
+            <section class="project-content">
+                <h3>B-rain website</h3>
+                <p>This was a website i made in my 1st semester at the study Frontend Design & Development for an actual client. Made with <strong>Html, css, Javascript</strong>
+                </p>
+                <a class="project-button" href="https://tristankatte.github.io/Brain-website/" target="_blank">To project</a>
+            </section>
         </article>
 
-                <article class="project-card">
-            <a href="https://ebok1.github.io/your-tribe-squad-page/" target="_blank">
-                <h3>Squad Page</h3>
-                <enhanced:img src="/static/images/squadpage.png" alt="" />
-            </a>
-            <div> 
-                <strong>HTML, CSS</strong>
-            </div>
+        <article class="project-card">
+            <section class="project-image">
+                
+                <enhanced:img src="/static/images/redpers.png" alt="">
+            </section>
+            <section class="project-content">
+                <h3>Redpers website</h3>
+                <p>A newswebsite that i made in a group project in my 3rd semester at the study Frontend Design & Development. Made with <strong>Sveltekit, WPAPI</strong>
+                </p>
+                <a class="project-button" href="https://redpers.agency.fdnd.nl/" target="_blank">To project</a>
+            </section>
         </article>
 
-                <article class="project-card">
-           
-            <a href="https://ebok1.github.io/your-tribe-squad-page/" target="_blank">
-                <h3>prototype website</h3>
-                <enhanced:img src="/static/images/squadpage.png" alt="" />
-            </a>
-            <div> 
-                <strong>HTML, CSS</strong>
-            </div>
+        <article class="project-card">
+            <section class="project-image">
+                <enhanced:img src="/static/images/informaat.png" alt="">
+            </section>
+            <section class="project-content">
+                <h3>Informaat website</h3>
+                <p>An assignment i got in the final sprint of my 1st semester. Here, I had to make a desktop-first website into a mobile-first design given from a design file. Made with <strong>Html, CSS</strong>
+                </p>
+                <a class="project-button" href="https://tristankatte.github.io/Informaat-opdracht/" target="_blank">To project</a>
+            </section>
+        </article>
+
+        <article class="project-card">
+            <section class="project-image">
+                <enhanced:img src="/static/images/squadpage.png" alt="">
+            </section>
+            <section class="project-content">
+                <h3>Squad page</h3>
+                <p>During the 1st sprint of the study, we got a group assignment to build a fun squadpage. My team had the idea to build a slot machine where you can actually run the machine and the students that you got, 
+                    had also a link to their digital business card.. Made with <strong>Html, CSS, Javascript</strong>
+                </p>
+                <a class="project-button" href="https://ebok1.github.io/your-tribe-squad-page/" target="_blank">To project</a>
+            </section>
+        </article>
+
+        <article class="project-card">
+            <section class="project-image">
+                <enhanced:img src="/static/images/prototype-1.png" alt="">
+            </section>
+            <section class="project-content">
+                <h3>Prototype website</h3>
+                <p>During the 2nd attempt at my final exam for the study Webdesign at the Grafisch Lyceum Utrecht, I had to design and build a prototype website for Wable.
+                    They are committed to KPAG activities with the aim of providing communities in Kenya, East Africa and the rest of the world with nature-based, economically viable solutions.
+                     Made with <strong>Wordpress, Elementor</strong>
+                </p>
+                <a class="project-button" href="https://tristankatte.nl/nieuw_portfolio/project-educatief-platform-wable/" target="_blank">To project</a>
+            </section>
         </article>
         
     </section>
@@ -135,15 +164,16 @@
 <style>
 
 
-    h2, h3 {
+  h3 {
         color: var(--heading-color);
         font-family: "Azonix";
-        text-align: center;
+        text-align: left;
     }
 
     h2 {
         font-size: 2rem;
         margin-bottom: 2rem;
+        color: var(--heading-color);
     }
     
     article {
@@ -155,51 +185,134 @@
 
     .projects-grid {
         display: flex;
-        flex-direction: row;
+        flex-direction: column;
         flex-wrap: wrap;
         justify-content: center;
         align-items: normal;
         align-content: center;
+        scroll-behavior: smooth;
+        scrollbar-width: none;
+        margin-bottom:5em;
     }
 
     .project-card {
-        background: rgba(255, 255, 255, 0.1);
+        background: var(--project-card-color);
         padding: 1.5rem;
-        border-radius: 8px;
-        border: 1px solid rgba(255, 255, 255, 0.2);
-        transition: transform 0.3s ease;
+        border-radius: 50px;
+        border: 3px solid var(--btn-color);
+        transition: transform 0.5s ease-in-out;
         flex: 1 1 350px;
+        width: 50%;
         margin: .8rem;
+        display: flex;
+        flex-direction: row;
+        position: sticky;
+        top: 1.75em;
+        transform-origin: center top;
+    }
+
+    .project-card:nth-child(1) {
+        transform: translateY(-100px);
+    }
+
+    .project-card:nth-child(2){
+        transform: translateY(-50px);
+    }
+
+    .project-card:nth-child(3){
+        transform: translateY(0px);
+    }
+
+    .project-card:nth-child(4){
+        transform: translateY(50px);
+    }
+
+    .project-card:nth-child(5){
+        transform: translateY(100px);
+    }
+
+    .project-card:nth-child(6){
+        transform: translateY(150px);
+    }
+
+    .project-card:nth-child(7){
+        transform: translateY(200px);
+    }
+
+    .project-card:nth-child(8){
+        transform: translateY(250px);
+    }
+
+    .project-image {
+        width: 100%;
+        height: 100%;
+        object-fit: cover;
+        margin-right: 2em;
+    }
+
+    .project-content p {
+        max-width: 800px;
+        margin-bottom: 1.5em;
+    }
+
+    strong {
+        color: var(--strong-color);
+        font-size: .95em;
+        font-style: italic;
+    }
+
+    .project-button {
+        position: relative;
+        padding: 15px 45px;
+        width:50%;
+        background: transparent;
+        border-radius: 0 50px 0 50px;
+        border: 2px solid #00c2cb;
+        outline: 2px solid #00c2cb;
+        outline-offset: -2px;
+        font-size: .95rem;
+        font-family: 'Orbitron Variable', sans-serif;
+        color: #00c2cb;
+        font-weight: 500;
+        text-decoration: none;
+        text-align: center;
+        cursor: pointer;
+        transition: outline-offset 200ms ease;
+    }
+
+    .project-button:hover {
+        color: #e0ffff;
+        background-color: var(--heading-color);
+        outline-offset: 4px;
     }
     
-    .project-card:hover {
+    /* .project-card:hover {
         transform: translateY(-5px);
-    }
+    } */
 
     .project-card h3 {
         font-size: 1.5rem;
         margin-bottom: 1rem;
+        align-items: flex-start;
     }
 
     .project-card a {
         text-decoration: none;
     }
 
-    img {
-	width: 100%;
-    margin-bottom: 1rem;
-}
 
 .grid-wrapper {
     columns: 300px;
     padding: 2em;
     display: flex;
     justify-content: center;
+    margin-bottom: .6em;
 }
 
 .columns {
 	display: grid;
 	grid-template-columns: repeat(3, 1fr);
+    
     
 }
 
@@ -207,6 +320,7 @@
 	display: flex;
 	flex-direction: column;
     margin: .6em;
+    
 }
 
 .column-reverse {
@@ -237,11 +351,6 @@
 	animation: adjust-position linear forwards;
 	animation-timeline: scroll(root block);
 }
-
-
-
-
-
 
 
 </style>
