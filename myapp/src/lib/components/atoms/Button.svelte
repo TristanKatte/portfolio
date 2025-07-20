6973d91c2082255120611fab712f7fe1f9ce30ce
TristanKatte <tristankatte@hotmail.com>
2025-07-20 19:42:13 +0200

    updating button component

--- myapp/src/lib/components/atoms/Button.svelte
+++ myapp/src/lib/components/atoms/Button.svelte
@@ -4,3 +4,36 @@
 </script>
 
 <a href={href} class="cta-button">{label}</a>
+
+
+<style>
+
+  .cta-button {
+  position: relative;
+  padding: 15px 45px;
+  width: 100%;
+  background: transparent;
+  border-radius: 0 50px 0 50px;
+  border: 2px solid #00c2cb;
+  outline: 2px solid #00c2cb;
+  outline-offset: -2px;
+  font-size: .95rem;
+  font-family: 'Orbitron Variable', sans-serif;
+  color: #00c2cb;
+  font-weight: 500;
+  text-decoration: none;
+  text-align: center;
+  cursor: pointer;
+  transition: outline-offset 200ms ease;
+  }
+
+.cta-button:hover {
+  color: #e0ffff;
+  background-color: var(--heading-color);
+  outline-offset: 4px;
+  }
+
+.cta-button:active{
+  transform: scale(0.95);
+  }
+</style>
