bb5c9e5999058107e63dfc7519a4820992c1b121
TristanKatte <tristankatte@hotmail.com>
2025-08-15 20:13:09 +0200

    changed config for hostinger

--- myapp/svelte.config.js
+++ myapp/svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-vercel';
+import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -6,7 +6,12 @@ const config = {
   preprocess: vitePreprocess(),
 
   kit: {
-    adapter: adapter(),
+    adapter: adapter({
+      pages: 'build',
+      assets: 'build',
+      fallback: 'index.html', // important for SPA routing
+      precompress: false
+    })
     // If your site is hosted in a subfolder on Hostinger, uncomment and set it here:
     // paths: { base: '/subfolder' }
   }
