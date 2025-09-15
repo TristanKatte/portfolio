462b7df2615349adc98af7dd55db14dfa87658f3
TristanKatte <tristankatte@hotmail.com>
2025-09-15 16:29:23 +0200

    installed prismic with temporary folder

---
+++ src/lib/components/SliceZone.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+  import { SliceZone } from '@prismicio/svelte';
+  import { components } from '$lib/slices';
+  export let slices;
+</script>
+
+<SliceZone {slices} {components} />
