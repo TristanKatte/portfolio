0c3e298f768a137f4446030ec52e573c571577f3
TristanKatte <tristankatte@hotmail.com>
2026-03-07 02:38:32 +0100

    fix: removed old prismic files

--- src/lib/components/SliceZone.svelte
+++
@@ -1,7 +0,0 @@
-<script lang="ts">
-  import { SliceZone } from '@prismicio/svelte';
-  import { components } from '$lib/slices';
-  export let slices: any[] = [];
-</script>
-
-<SliceZone {slices} {components} />
--- src/routes/slice-simulator/+page.svelte
+++
@@ -1,6 +0,0 @@
-<script lang="ts">
-  import { SliceSimulator } from '@prismicio/svelte/slice-simulator';
-  import { components } from '$lib/slices';
-</script>
-
-<SliceSimulator {components} />
